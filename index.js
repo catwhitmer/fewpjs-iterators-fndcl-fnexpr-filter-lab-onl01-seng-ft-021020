ebdd7554de4ad44c3c0ecd6e1e63fed6894bfa89
Cathrin Whitmer <catwhitmer@gmail.com>
2020-05-23 18:46:09 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -6,17 +6,17 @@ function findMatching(drivers, name){
 }
 
 function fuzzyMatch(drivers, letter){
-  return drivers.filter( find_driver => {
-    if (letter.charAt(0) === find_driver.charAt(0)){
+  return drivers.filter( find_name => {
+    if (letter.charAt(0) === find_name.charAt(0)){
     return find_name
     }
   })
 }
 
 function matchName(drivers, name){
-  return drivers.filter( function(find_driver){
+  return drivers.filter( function(find_name){
     if (find_name.name == name){
-      return find_driver
+      return find_name
     }
   })
 }
