da2315223ff7e9bbf9179b38346136921cbb2804
Cathrin Whitmer <catwhitmer@gmail.com>
2020-05-23 18:44:54 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,6 +1,24 @@
-function findMatching(drivers, name) {
-    return drivers.filter(find_driver => {
-        return (find_driver.toLowerCase() === name.toLowerCase())
-    }
+function findMatching(drivers, name){
+  return drivers.filter( find_driver => {
+    return (find_driver.toLowerCase() === name.toLowerCase())
+   }
   )
 }
+
+function fuzzyMatch(drivers, letter){
+  return drivers.filter( find_driver => {
+    if (letter.charAt(0) === find_driver.charAt(0)){
+    return find_name
+    }
+  })
+}
+
+function matchName(drivers, name){
+  return drivers.filter( function(find_driver){
+    if (find_name.name == name){
+      return find_driver
+    }
+  })
+}
+    
+    
